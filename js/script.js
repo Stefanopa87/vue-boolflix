70ef6ac3621bb00d17729ea84f75f0fa8f471700
Stefano Patti <mastepa@hotmail.com>
2021-04-19 15:55:00 +0200

    search films done

--- js/script.js
+++ js/script.js
@@ -5,37 +5,72 @@ function init() {
         el: '#app',
         data: {
 
-            films: []
+            films: [],
+
+            searchFilms: ""
         },
 
         methods: {
 
+            search: function () {
+
+
+                if (this.searchFilms.length > 0) {
+
+                    console.log(this.searchFilms);
+
+                    axios.get('https://api.themoviedb.org/3/search/movie', {
+
+                            params: {
+                                'api_key': '8c6e58cb6f1459e345a3ff802e48cdba',
+                                'query': this.searchFilms,
+                                'language': 'it',
+                            }
+                        })
+
+                        .then(data => {
+
+                            let results = data.data.results;
+
+                            this.films = results;
+
+                            console.log(this.films)
+                        })
+
+                        .catch(() => console.log('error'));
+
+                }
+
+                this.searchFilms = ""
+            }
+
         },
 
-        mounted() {
-            axios.get('https://api.themoviedb.org/3/search/movie', {
+        // mounted() {
+            // axios.get('https://api.themoviedb.org/3/search/movie', {
 
-                    params: {
-                        'api_key': '8c6e58cb6f1459e345a3ff802e48cdba',
-                        'query': 'ritorno al futuro'
-                    }
-                })
+            //         params: {
+            //             'api_key': '8c6e58cb6f1459e345a3ff802e48cdba',
+            //             'query': this.searchFilms,
+            //             'language': 'it',
+            //         }
+            //     })
 
 
-                .then(data => {
+            //     .then(data => {
 
-                    let results = data.data.results;
+            //         let results = data.data.results;
 
-                    this.films = results;
+            //         this.films = results;
 
-                    console.log(this.films)
+            //         console.log(this.films)
 
-                    console.log('ciao');
+            //         console.log('ciao');
 
-                })
+            //     })
 
-                .catch(() => console.log('error'));
-        }
+            //     .catch(() => console.log('error'));
+        // }
     });
 }
 
